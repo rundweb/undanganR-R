60311db60c51761adc7428e8ffd262d5ff919f6f
faldo <pn4066283@gmail.com>
2026-02-23 11:01:31 +0700

    done texts iis

--- src/components/FirstComponents.jsx
+++ src/components/FirstComponents.jsx
@@ -16,7 +16,7 @@ const FirstComponents = () => {
         </div>
         <div className='flex items-center justify-center flex-col'>
             <p data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="200" className='font-Sub font-semibold tracking-wide text-sm text-emerald-950 mb-4'>WEDDING INVITATION</p>
-            <h1 data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="400" className='font-Heading text-5xl tracking-wide text-amber-900 mb-2'>Risma & Raka</h1>
+            <h1 data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="400" className='font-Heading text-5xl tracking-wide text-amber-900 mb-2'>Iis & Raka</h1>
             <h2 data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="600" className='font-Body text-sm tracking-wider text-emerald-950 mb-5'>Rabu, 25 Maret 2026 </h2>
             <a data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="800" data-aos-offset="1" href="SecondLook.jsx" className='flex items-center font-Body bg-amber-950/80 w-max py-2.5 px-6 rounded-full text-sm tracking-wide text-white gap-2'><FaEnvelopeOpenText />Buka Undangan</a>
         </div>
